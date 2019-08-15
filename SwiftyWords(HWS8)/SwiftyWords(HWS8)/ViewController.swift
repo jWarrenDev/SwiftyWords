76e893fb3cc9c9622bda175ddcf177ab0159ae8e
Jerrick Warren <Jerrick.Warren@gmail.com>
2019-08-15 08:55:03 -0700

    initial Commit

---
+++ SwiftyWords(HWS8)/SwiftyWords(HWS8)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyWords(HWS8)
+//
+//  Created by Jerrick Warren on 8/15/19.
+//  Copyright © 2019 Jerrick Warren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
